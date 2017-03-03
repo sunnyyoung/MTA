a57413d4d9e886eea8244100fa5b69b96f1db457
Sunnyyoung <iSunnyyoung@gmail.com>
2017-03-03 16:16:49 +0800

    Sync MTA

--- SDK/MTAConfig.h
+++ SDK/MTAConfig.h
@@ -1,67 +1,213 @@
 //
-//  StatConfig.h
+//  MTAConfig.h
 //  TA-SDK
 //
-//  Created by WQY on 12-11-5.
-//  Copyright (c) 2012年 WQY. All rights reserved.
+//  Originally created by WQY on 12-11-5.
+//  Updated and maintained by xiangchen and tyzualtang.
 //
 
 #import <Foundation/Foundation.h>
 
+/**
+ 上报策略枚举值
+ */
 typedef enum {
-    MTA_STRATEGY_INSTANT = 1,            //实时上报
-    MTA_STRATEGY_BATCH = 2,              //批量上报，达到缓存临界值时触发发送
-    MTA_STRATEGY_APP_LAUNCH = 3,         //应用启动时发送
-    MTA_STRATEGY_ONLY_WIFI = 4,          //仅在WIFI网络下发送
-    MTA_STRATEGY_PERIOD = 5,             //每间隔一定最小时间发送，默认24小时
-    MTA_STRATEGY_DEVELOPER = 6,          //开发者在代码中主动调用发送行为
-    MTA_STRATEGY_ONLY_WIFI_NO_CACHE = 7  //仅在WIFI网络下发送, 发送失败以及非WIFI网络情况下不缓存数据
+	/**
+	 实时上报
+	 */
+    MTA_STRATEGY_INSTANT = 1,
+
+	/**
+	 批量上报，达到缓存临界值时触发发送
+	 */
+    MTA_STRATEGY_BATCH = 2,
+
+	/**
+	 应用启动时发送
+	 */
+    MTA_STRATEGY_APP_LAUNCH = 3,
+
+	/**
+	 仅在WIFI网络下发送
+	 */
+    MTA_STRATEGY_ONLY_WIFI = 4,
+
+	/**
+	 每间隔一定最小时间发送，默认24小时
+	 */
+    MTA_STRATEGY_PERIOD = 5,
+
+	/**
+	 开发者在代码中主动调用发送行为
+	 */
+    MTA_STRATEGY_DEVELOPER = 6,
+
+	/**
+	 仅在WIFI网络下发送, 发送失败以及非WIFI网络情况下不缓存数据
+	 */
+    MTA_STRATEGY_ONLY_WIFI_NO_CACHE = 7
 } MTAStatReportStrategy;
 
 @interface MTAConfig : NSObject
 
+#pragma mark - 常规配置项
+
 /**
- *  debug开关
- *	打开以后,终端会输出debug日志
- *	同时日志也会记录在沙箱Documents目录下.mtaLog文件中
+ 取得MTA配置的共享实例
+ 修改实例的属性必须在调用MTA启动函数之前执行
+
+ @return MTAConfig的共享实例
+ */
++(instancetype) getInstance;
+
+/**
+ debug开关
+ 开以后，终端会输出debug日志，默认关闭
  */
 @property BOOL debugEnable;
 
+/**
+ Session过期时间，默认30秒
+ */
+@property uint32_t sessionTimeoutSecs;
 
-@property uint32_t sessionTimeoutSecs;          //Session超时时长，默认30秒
-@property (nonatomic) MTAStatReportStrategy reportStrategy;    //统计上报策略
-@property (nonatomic, copy) NSString* appkey; //应用的统计AppKey
-@property (nonatomic, copy) NSString* channel;//渠道名，默认为"appstore"
-@property uint32_t maxStoreEventCount;          //最大缓存的未发送的统计消息，默认1024
-@property uint32_t maxLoadEventCount;           //一次最大加载未发送的缓存消息，默认30
-@property uint32_t minBatchReportCount;         //统计上报策略为BATCH时，触发上报时最小缓存消息数，默认30
-@property uint32_t maxSendRetryCount;           //发送失败最大重试数，默认3
-@property uint32_t sendPeriodMinutes;           //上报策略为PERIOD时发送间隔，单位分钟，默认一天（1440分钟）
-@property uint32_t maxParallelTimingEvents;     //最大并行统计的时长事件数，默认1024
-@property BOOL  smartReporting;                 //智能上报开关：在WIFI模式下实时上报，默认TRUE
-@property BOOL  autoExceptionCaught;            //智能捕获未catch的异常，默认TRUE；设置为False需要在startWithAppkey前调用
-@property uint32_t maxReportEventLength;        //最大上报的单条event长度，超过不上报
-@property (nonatomic, copy) NSString* qq;           //QQ号或者帐号
-@property (nonatomic, copy) NSString* account;      //帐号
-@property int8_t accountType;                       //帐号类型
-@property (nonatomic, copy) NSString* accountExt;   //帐号的扩展信息
+/**
+ 上报策略
+ */
+@property (nonatomic) MTAStatReportStrategy reportStrategy;
+
+/**
+ 应用的统计AppKey
+ */
+@property (nonatomic, copy) NSString* appkey;
+
+/**
+ 渠道名，默认为"appstore"
+ */
+@property (nonatomic, copy) NSString* channel;
+
+
+/**
+ 最大缓存的未发送的统计消息，默认1024条
+ */
+@property uint32_t maxStoreEventCount;
+
+/**
+ 一次最大加载未发送的缓存消息，默认30条
+ */
+@property uint32_t maxLoadEventCount;
+
+/**
+ 统计上报策略为BATCH时，触发上报时最小缓存消息数，默认30条
+ */
+@property uint32_t minBatchReportCount;
+
+/**
+ 发送失败最大重试数，默认3次
+ */
+@property uint32_t maxSendRetryCount;
+
+/**
+ 上报策略为PERIOD时发送间隔，单位分钟，默认一天（1440分钟）
+ */
+@property uint32_t sendPeriodMinutes;
+
+/**
+ 允许同时统计的时长事件数，默认1024条
+ */
+@property uint32_t maxParallelTimingEvents;
+
+/**
+ 智能上报
+ 开启以后设备接入WIFI会实时上报
+ 否则按照全局策略上报
+ 默认打开
+ */
+@property BOOL  smartReporting;
+
+/**
+ 是否启动MTA的崩溃报告功能
+ 默认为YES
+ */
+@property BOOL  autoExceptionCaught;
+
+/**
+ 最大上报的单条event长度，超过不上报
+ 单位Byte
+ 默认4096，即4KB
+ */
+@property uint32_t maxReportEventLength;
+
+/**
+ QQ号
+ 调用MTA reportQQ方法后会设置此属性
+ 默认为空
+ */
+@property (nonatomic, copy) NSString* qq;
+
+/**
+ MTA是否启动
+ */
 @property BOOL statEnable;
 
-@property (nonatomic, copy) NSString* customerUserID;
-@property (nonatomic, copy) NSString* customerAppVersion;
+/**
+ 设备的idfa，建议有广告权限的app设置此字段
+ 默认为空
+ */
 @property (nonatomic, copy) NSString* ifa;
-@property (nonatomic, copy) NSString* pushDeviceToken;
 
-@property (nonatomic, copy) NSString* statReportURL; //自定义的上报url
+/**
+ 用户自定义的App版本
+ 默认为空
+ 若设置了这个字段，SDK会取这个字段作为APP版本上报
+ 若没设置，则取工程配置文件中的‘Bundle versions string, short’字段作为APP版本上报
+ 若没没设置‘Bundle versions string, short’字段，则取‘Bundle version’字段上报
+ */
+@property (nonatomic, copy) NSString* customerAppVersion;
+
+/**
+ 一个Session内允许上报的最大事件数
+ 传入-1表示无限制
+ 不建议修改这个字段
+ 默认无限制
+ */
 @property int32_t maxSessionStatReportCount;
 
-@property (nonatomic,copy) NSString* op;          //运营商
-@property (nonatomic,copy) NSString* cn;          //网络类型
+/**
+ 崩溃报告回调函数
+ 在发生崩溃后的下次启动时调用
 
+ @param NSString 崩溃报告，JSON格式
+ */
 typedef void (^errorCallback)(NSString *);
+
+/**
+ 崩溃报告的回调函数
+ */
 @property (nonatomic,copy) errorCallback crashCallback; //用于crash日志删除前回调，将在崩溃发生后下次启动时调用， param为crash JSON数据
+
+/**
+ 获取在MTA前端控制台配置的参数
+ 调用这个函数前需要在MTA前端控制台中‘应用配置管理项’下的‘自定义参数’中配置才能生效
+ 首次配置或者更改参数配置后，需要3-5分钟才能生效
+
+ @param key 参数的key
+ @param v 没取到参数时返回的默认值
+ @return 参数的值或者默认值
+ */
+-(NSString*)getCustomProperty:(NSString*) key default:(NSString*) v;
+
+#pragma mark - 高级配置项，具体配置方法请咨询客服
+
+@property (nonatomic, copy) NSString* statReportURL;
+@property (nonatomic, copy) NSString* customerUserID;
+@property (nonatomic, copy) NSString* account;
+@property int8_t accountType;
+@property (nonatomic, copy) NSString* accountExt;
+@property (nonatomic, copy) NSString* pushDeviceToken;
+@property (nonatomic,copy) NSString* op;
+@property (nonatomic,copy) NSString* cn;
+
 //typedef void (^crashCallback) (void);
 //@property (nonatomic,copy) crashCallback atCrashCallback; // 崩溃发生时候的回调，将在崩溃发生时候调用
--(NSString*)getCustomProperty:(NSString*) key default:(NSString*) v;
-+(instancetype) getInstance;
 @end
